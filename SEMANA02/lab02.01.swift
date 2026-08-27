c8721eba9b5f6de53507dbafd212145a0a938534
Jeronimo Ortiz <jeronimo.ortiz@tecsup.edu.pe>
2026-08-27 12:26:53 -0500

    Definiendo promedio de pesos

---
+++ SEMANA02/lab02.01.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+//Definir promedio de pesos
+let parcial = 0.3
+let trabajo = 0.3
+let final = 0.4
+
+print("Nombre del Alumno")
+let alumno = readLine() ?? ""
+print ("Nota del Examen Parcial : ")
+let exparcial = Double(readLine() ?? "") ?? 0.0
