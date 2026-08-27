64b50e0a61f39eed5f9092bfe9b987cbc6b18764
Jeronimo Ortiz <jeronimo.ortiz@tecsup.edu.pe>
2026-08-27 12:04:26 -0500

    add suma de numeros

---
+++ SEMANA02/lab02.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+print("Number 1")
+let a = Double(readLine() ?? "") ?? 0.0
+
+print("Number 2")
+let b = Double(readLine() ?? "") ?? 0.0
+
+let suma = a + b
+print("La suma es \(suma)")
