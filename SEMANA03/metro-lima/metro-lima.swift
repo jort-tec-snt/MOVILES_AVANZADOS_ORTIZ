7711c9e8efef7f1a010d3f9bee4886f012cc3832
Jeronimo Ortiz <jeronimo.ortiz@tecsup.edu.pe>
2026-09-06 11:24:24 -0500

    chore: inicializando estructura base del simulador

---
+++ SEMANA03/metro-lima/metro-lima.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+//Modelo de Datos
+
+struct LineaMetro {
+    let codigo: String;
+    let nombre: String;
+    let estado: String;
+    let origen: String;
+    let destino: String;
+    let estaciones: [String];
+}
+
+
